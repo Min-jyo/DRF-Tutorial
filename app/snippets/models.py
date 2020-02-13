79d0b4547b2db288527b3f18b3a9ad87a9346c25
이한영 <dev@lhy.kr>
2020-02-13 13:42:24 +0900

    Snippet.author필드 추가 및 DB migration수정

--- app/snippets/models.py
+++ app/snippets/models.py
@@ -10,8 +10,8 @@ STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
 
 
 class Snippet(models.Model):
-    # author = models.ForeignKey(
-    #     settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True)
     code = models.TextField()
